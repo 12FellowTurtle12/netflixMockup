8be55a7dfae69cb129158069443e8348cef36221
Kerim <kerimcanak@gmail.com>
2024-06-11 11:35:25 +0300

    configurated profiles and movie interface

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { userSuggestionsData } from "../userSuggestions";
+import Suggestion from "../components/Suggestion/Suggestion";
+
+/* eslint-disable react/prop-types */
+export default function Home(props) {
+  const [suggestions, setSuggestions] = useState([]);
+  const { activeProfile } = props;
+  const { name } = activeProfile;
+
+  useEffect(() => {
+    const suggestionData = userSuggestionsData[activeProfile.id];
+    setSuggestions(suggestionData);
+  }, [activeProfile]);
+
+  return (
+    <>
+      <div>Home {name}</div>{" "}
+      {suggestions.map((suggestion, index) => {
+        return <Suggestion key={index} suggestion={suggestion} />;
+      })}
+    </>
+  );
+}
